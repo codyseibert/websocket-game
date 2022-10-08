5f6629a59117f411722cad169158adff03b2d28f
Cody Seibert <cseibert113@gmail.com>
2022-10-07 22:12:51 -0400

    moving player

--- public/index.js
+++ public/index.js
@@ -1,11 +1,35 @@
 const socket = io();
 
-const pingButton = document.getElementById("ping");
+const upButton = document.getElementById("up");
+const downButton = document.getElementById("down");
+const leftButton = document.getElementById("left");
+const rightButton = document.getElementById("right");
 
-pingButton.addEventListener("click", () => {
-  socket.emit("ping");
+upButton.addEventListener("click", () => {
+  socket.emit("up");
 });
 
-socket.on("pong", (message) => {
-  console.log("pong", message);
+downButton.addEventListener("click", () => {
+  socket.emit("down");
+});
+
+leftButton.addEventListener("click", () => {
+  socket.emit("left");
+});
+
+rightButton.addEventListener("click", () => {
+  socket.emit("right");
+});
+
+const playerEl = document.getElementById("player");
+const foodEl = document.getElementById("food");
+
+socket.on("player", (player) => {
+  playerEl.style.left = player.left + "px";
+  playerEl.style.top = player.top + "px";
+});
+
+socket.on("food", (food) => {
+  foodEl.style.left = food.left + "px";
+  foodEl.style.top = food.top + "px";
 });
