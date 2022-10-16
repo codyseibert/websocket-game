859d589d29fa41d5edb299f3e78e9b9be72bb789
Cody Seibert <cseibert113@gmail.com>
2022-10-16 10:06:22 -0400

    spawning coins

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const random = require("random-name");
 const GRAVITY = 0.0228;
 const TICK_RATE = 30;
 const TILE_SIZE = 32;
+const COIN_SIZE = 6;
 const PLAYER_SPEED = 5.0;
 const PLAYER_SIZE = 16;
 const CONTROLS = {
@@ -56,6 +57,7 @@ for (let row = 0; row < map.length; row++) {
   }
 }
 
+let coins = [];
 let players = [];
 const playerSocketMap = {};
 const controlsMap = {};
@@ -89,6 +91,7 @@ io.on("connect", (socket) => {
     y: 100,
     vx: 0,
     vy: 0,
+    score: 0,
     name: random.first(),
     id: socket.id,
     color: `#${Math.floor(Math.random() * (0xffffff + 1)).toString(16)}`,
@@ -132,6 +135,7 @@ const getBoundingBoxFactory = (STATIC_SIZE) => (entity) => {
 
 const getPlayerBoundingBox = getBoundingBoxFactory(PLAYER_SIZE);
 const getTileBoundingBox = getBoundingBoxFactory(TILE_SIZE);
+const getCoinBoundingBox = getBoundingBoxFactory(COIN_SIZE);
 
 const isCollidingWithMap = (player) => {
   for (const collidable of collidables) {
@@ -144,9 +148,28 @@ const isCollidingWithMap = (player) => {
   return false;
 };
 
+const spawnCoin = () => {
+  const randomRow = Math.floor(Math.random() * map.length);
+  const randomCol = Math.floor(Math.random() * map[0].length);
+  if (map[randomRow][randomCol] !== 0) return;
+  coins.push({
+    x: randomCol * TILE_SIZE,
+    y: randomRow * TILE_SIZE,
+  });
+};
+setInterval(spawnCoin, 1000);
+
 const tick = (delta) => {
   for (const player of players) {
     const playerControls = controlsMap[player.id] ?? {};
+
+    for (let i = coins.length - 1; i >= 0; i--) {
+      const coin = coins[i];
+      if (isOverlap(getCoinBoundingBox(coin), getPlayerBoundingBox(player))) {
+        player.score++;
+        coins.splice(i, 1);
+      }
+    }
 
     if (playerControls[CONTROLS.RIGHT]) {
       player.x += PLAYER_SPEED;
@@ -161,13 +184,15 @@ const tick = (delta) => {
         player.x += PLAYER_SPEED;
       }
     }
+
     player.vy += GRAVITY * delta;
     player.y += player.vy;
-
     if (isCollidingWithMap(player)) {
+      if (player.vy > 0) {
+        canJump[player.id] = true;
+      }
       player.y -= player.vy;
       player.vy = 0;
-      canJump[player.id] = true;
     }
 
     if (playerControls[CONTROLS.JUMP] && canJump[player.id]) {
@@ -177,6 +202,7 @@ const tick = (delta) => {
   }
 
   io.emit("players", players);
+  io.emit("coins", coins);
 };
 
 let lastUpdate = Date.now();
